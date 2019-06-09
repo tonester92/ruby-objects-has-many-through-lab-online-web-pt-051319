909e94ffdadc4d47f73f65b4928b051d7d00d9f8
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-09 17:37:45 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,10 @@
 class Patient
   attr_reader :name, :appointment, :doctor
+  @@all = []
   
   def initialize(name)
     @name = name
+    @@all << self 
   end
 
   def new_appointment(doctor, date)
