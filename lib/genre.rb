da8a1169c2fff1486741afe9b429789ae02ba18d
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-09 17:39:32 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,15 +1,21 @@
 class Genre
   attr_reader :name, :genre
   attr_accessor :song
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def songs
     Song.all.select do |song|
       song.genre == self
     end
+  end
+
+  def self.all
+    @@all
   end
 
   def artists
